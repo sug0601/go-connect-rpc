e14357842993fc4edf76ab3841726bf3e14256bd
sug0601 <sug0601@live.jp>
2025-10-03 12:00:55 +0900

    colorlize

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.23.0
 
 require (
 	github.com/bufbuild/connect-go v1.10.0
+	github.com/fatih/color v1.18.0
+	github.com/google/uuid v1.6.0
 	github.com/uptrace/bun v1.2.15
 	github.com/uptrace/bun/dialect/pgdialect v1.2.15
 	github.com/uptrace/bun/driver/pgdriver v1.2.15
@@ -11,8 +13,9 @@ require (
 )
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
--- src/middleware/logging_interceptor.go
+++ src/middleware/logging_interceptor.go
@@ -2,31 +2,43 @@ package middleware
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 
 	"github.com/bufbuild/connect-go"
+	"github.com/fatih/color"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 )
 
-// LoggingInterceptor は Connect v1 用
 func LoggingInterceptor() connect.UnaryInterceptorFunc {
+	marshaler := protojson.MarshalOptions{
+		EmitUnpopulated: true,
+	}
+
 	return func(next connect.UnaryFunc) connect.UnaryFunc {
 		return func(
 			ctx context.Context,
 			req connect.AnyRequest,
 		) (connect.AnyResponse, error) {
 
-			reqJSON, _ := json.Marshal(req.Any())
-			fmt.Printf("[RPC] Request: %s\n", reqJSON)
+			color.New(color.FgYellow).Printf("[RPC] Procedure: %s\n", req.Spec().Procedure)
+
+			if reqMsg, ok := req.Any().(proto.Message); ok {
+				if reqJSON, err := marshaler.Marshal(reqMsg); err == nil {
+					color.New(color.FgCyan).Printf("[RPC] Request: %s\n", reqJSON)
+				}
+			}
 
 			res, err := next(ctx, req)
 			if err != nil {
-				fmt.Printf("[RPC] Error: %v\n", err)
+				color.New(color.FgRed).Printf("[RPC] Error: %v\n", err)
 				return nil, err
 			}
 
-			resJSON, _ := json.Marshal(res.Any())
-			fmt.Printf("[RPC] Response: %s\n", resJSON)
+			if resMsg, ok := res.Any().(proto.Message); ok {
+				if resJSON, err := marshaler.Marshal(resMsg); err == nil {
+					color.New(color.FgGreen).Printf("[RPC] Response: %s\n", resJSON)
+				}
+			}
 
 			return res, nil
 		}
